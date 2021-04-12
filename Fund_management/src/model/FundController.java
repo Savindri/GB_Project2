a59402a64704b9adc4a709a51f7fe0bd2eca5d44
buddhika9 <it19223186@my.sliit.lk>
2021-04-13 04:54:02 +0530

    Add a Common DB class

--- Fund_management/src/model/FundController.java
+++ Fund_management/src/model/FundController.java
@@ -3,28 +3,26 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+
+import com.mysql.cj.xdevapi.Statement;
+import util.DBConnect;
 
 public class FundController {
 	
-	/* Database connection */
-	private static String url = "127.0.0.1:3306/gadetbadget";
-	private static String username = "root";
-	private static String password = "";
 	
-	//Creating Database Connection
-	private static Connection connect() {
-		Connection con = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-
-			// Provide the correct details: DBServer/DBName, username, password
-			con = DriverManager.getConnection("jdbc:mysql://" + url, username, password);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return con;
-	}
 	
 	
 	 public String insertFund(int fundid , int cusid , String fund_desc , String fundamt, String requestDate, String status) {
@@ -33,7 +31,7 @@ public class FundController {
 			
 			try{ 
 				
-		     Connection con = connect();
+		     Connection con = DBConnect.connect();
 			
 			if (con == null) {
 				return"Error while connecting to the database for inserting."; 
@@ -68,6 +66,7 @@ public class FundController {
 			
 			// execute the statement
 			preparedStmt.execute(); 
+			
 			con.close();
 			output = "Fund ID = '"+fundid+"' - Details inserted successfully";
 			
@@ -90,7 +89,7 @@ public class FundController {
 	 	String output = "";
 
 	 	try{ 
-	 		Connection con = connect();
+	 		 Connection con = DBConnect.connect();
 	 	
 	 	if (con == null) {
 	 		
@@ -122,12 +121,74 @@ public class FundController {
 	 	} 
 	 
 	 
+	 public String readFundDetails()
+		{
+			String output = "";
+			try
+			{
+				 Connection con = DBConnect.connect();
+			if (con == null)
+			{return "Error while connecting to the database for reading."; }
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr>"
+					  + "<th>Fund ID</th>"
+					  + "<th>Fund Description</th>" 
+					  + "<th>Fund amount</th>" 
+					  +	"<th>Fund Request Date</th>" 
+					  +	"<th>Fund Request Status</th>" 
+					  +	"<th>Fund Grant Date</th>" 
+					  + "<th>Update</th><th>Remove</th></tr>";
+			
+			String query = "select * from product";
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+			ResultSet rs = preparedStmt.executeQuery(query);
+			
+			// iterate through the rows in the result set
+			while (rs.next())
+			{
+				String FundID = Integer.toString(rs.getInt("FundID"));
+				String Funddesc = rs.getString("Fund_desc");
+				Double Fundamount = rs.getDouble("Fund_amount");
+				Date ReqDate = rs.getDate("Req_date");
+				Double ReqStatus = rs.getDouble("Req_status");
+				Date GrantDate = rs.getDate("Grant_date");
+			
+				
+				// Add into the html table
+				output += "<tr><td>" + FundID + "</td>";
+				output += "<td>" + Funddesc + "</td>";
+				output += "<td>" + Fundamount + "</td>";
+				output += "<td>" + ReqDate + "</td>";
+				output += "<td>" + ReqStatus + "</td>";
+				output += "<td>" + GrantDate + "</td>";
+				
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+				        + "<td><form method='post' action='#'>"//need to add the redirect jsp
+						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+						+ "<input name='pro_ID' type='hidden' value='" + FundID+ "'>" + "</form></td></tr>";
+			}
+			con.close();
+			
+			// Complete the html table
+			output += "</table>";
+			}
+			catch (Exception e)
+			{
+				output = "Error while reading the products.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+	 
+	 
 		
 		//Deleting the fund details
 		public static String deleteFund(int fundid) {
 			String output = "";
 			try {
-				  Connection con = connect();
+				 Connection con = DBConnect.connect();
 				if (con == null) {
 					return "Error while connecting to the database for deleting.";
 				}
