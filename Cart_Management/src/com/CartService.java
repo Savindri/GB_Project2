66c823761279cf29b97d9b0e573c86adfcc6b6de
Savindri <savindriperera999@gmail.com>
2021-04-13 03:01:23 +0530

    Added service class and model class.

---
+++ Cart_Management/src/com/CartService.java
@@ -0,0 +1,19 @@
+package com;
+
+import model.Cart; 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+
+
+//To implement the RESTful API
+
+public class CartService {
+
+}
