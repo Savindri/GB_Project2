d5902fcfc2cf47fed3ae1a4b9ebd36fdf44f2f4f
it19157306 <it19157306@my.sliit.lk>
2021-04-13 14:00:30 +0530

    added insert method

--- storeManagement/src/model/store.java
+++ storeManagement/src/model/store.java
@@ -18,4 +18,37 @@ public class store {
 		return con;
 	}
 
+	public String insertProduct(String pro_ID, String desc, String qty, String price, String category)
+	{
+		String output = "";
+		try
+		{
+		Connection con = connect();
+		if (con == null)
+		{return "Error while connecting to the database for inserting."; }
+		
+		// create a prepared statement
+		String query = " insert into product(`pro_ID`,`desc`,`qty`,`price`,`category`)"
+		+ " values (?, ?, ?, ?, ?)";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		
+		// binding values
+		preparedStmt.setString(1, pro_ID);
+		preparedStmt.setString(2, desc);
+		preparedStmt.setInt(3, 2);
+		preparedStmt.setDouble(4, Double.parseDouble(price));
+		preparedStmt.setString(5, category);
+		
+		// execute the statement
+		preparedStmt.execute();
+		con.close();
+		output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+		output = "Error while inserting the products.";
+		System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }
