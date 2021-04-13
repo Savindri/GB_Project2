2ca76b83a61925eeede14b16656849abdbd3347a
it19157306 <it19157306@my.sliit.lk>
2021-04-13 14:15:40 +0530

    implement read product operation

--- storeManagement/src/com/storeService.java
+++ storeManagement/src/com/storeService.java
@@ -1,5 +1,30 @@
 package com;
 
+import model.store; 
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+//read operation
+@Path("/Test")
 public class storeService {
+	store strObj = new store(); 
+		
+		@GET
+		@Path("/") 
+		@Produces(MediaType.TEXT_HTML) 
+		public String readStore()
+		{ 
+			return strObj.readProduct(); 
+		}
 
 }
