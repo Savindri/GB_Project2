751c502f78408c2572596ec1457065ac0e5b6494
it19157306 <it19157306@my.sliit.lk>
2021-04-13 14:18:22 +0530

    implement insert operation

--- storeManagement/src/com/storeService.java
+++ storeManagement/src/com/storeService.java
@@ -26,5 +26,18 @@ public class storeService {
 		{ 
 			return strObj.readProduct(); 
 		}
-
+//insert operation
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertProduct(@FormParam("pro_ID") String pro_ID,
+									@FormParam("desc") String desc,
+									@FormParam("qty") String qty,
+									@FormParam("price") String price,
+									@FormParam("category") String category)
+		{
+			String output = strObj.insertProduct(pro_ID, desc, qty, price, category);
+			return output;
+		}
 }
